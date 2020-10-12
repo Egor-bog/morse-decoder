522264e23cc66592eefe4a000cf4638f6f416089
Egor-bog <7548494@gmail.com>
2020-10-12 23:38:53 +0300

    fix: task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let map = new Map (Object.entries(MORSE_TABLE));
+    const str = expr.match(new RegExp(`.{1,${10}}`, 'g'));
+    let resW = '';
+    let letter = '';
+    for (let i = 0; i < str.length; i++) {
+      let resS = '';
+      let world = str[i];
+      if (world == '**********') {
+        resW += " ";
+        continue;
+      }
+      for (let y = 0; y < world.length; y++) {
+        if (world[y] == 0) continue;
+        else {
+          world = world.slice(y);
+          break;
+        }
+      }  
+        worldStr = world.match(new RegExp(`.{1,${2}}`, 'g'));
+        for (let x = 0; x < worldStr.length; x++) {
+          if (worldStr[x] == '10') {resS += '.' ;} 
+          else {resS += '-';}
+        }  
+      letter =map.get(resS);
+      resW += letter;
+      
+    }
+    return resW;
 }
 
 module.exports = {
